22f7f9031c7878a96fd6c1eeabf42036d0c00e7b
jesse-dahl <jddahl199@gmail.com>
2019-06-28 15:51:21 -0600

    added Expanded function to reduce repetitive code. also re-finished the app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class XylophoneApp extends StatelessWidget {
       final player = AudioCache();
       player.play('note$sound.wav');
   }
+
+  Expanded buildKey({int sound, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(sound);
+        },
+      ),
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -20,68 +31,13 @@ class XylophoneApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
 
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playSound(1);
-                  },
-                ),
-              ),
-
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                ),
-              ),
-
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                ),
-              ),
-
-              Expanded(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                ),
-              ),
-
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                ),
-              ),
-
-              Expanded(
-                child: FlatButton(
-                  color: Colors.indigo,
-                  onPressed: () {
-                    playSound(6);
-                  },
-                ),
-              ),
-
-              Expanded(
-                child: FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                ),
-              ),
+              buildKey(sound: 1, color: Colors.red),
+              buildKey(sound: 2, color: Colors.orange),
+              buildKey(sound: 3, color: Colors.yellow),
+              buildKey(sound: 4, color: Colors.green),
+              buildKey(sound: 5, color: Colors.blue),
+              buildKey(sound: 6, color: Colors.indigo),
+              buildKey(sound: 7, color: Colors.purple),
 
             ],
           ),
